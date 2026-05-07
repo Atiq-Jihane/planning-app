0aee60aa1a56e8cfcc2d461ecb6057601a2a2d00
ATIQ <jihane.atiq@disney.com>
2026-05-07 14:44:03 +0200

    fix postgresqSQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
-MERGE INTO users (email, prenom, nom, equipe, role) KEY(email)
-    VALUES
-    ('jihane.atiq@disney.com',           'Jihane', 'Atiq',             'Developpement digital', 'ADMIN'),
-    ('louise.amaladasse@disney.com',     'Louise', 'Amaladasse',       'Developpement digital', 'USER'),
-    ('alba.avesada.calderon@disney.com', 'Alba',   'Avesada Calderon', 'Dining',                'USER'),
-    ('cyril.macaluso@disney.com',        'Cyril',  'Macaluso',         'Dining',                'USER'),
-    ('marie.dupont@disney.com',          'Marie',  'Dupont',           'Developpement digital', 'USER');
+-- ✅ PostgreSQL syntax
+INSERT INTO users (email, prenom, nom, equipe, role)
+VALUES
+    ('jihane.atiq@disney.com', 'Jihane', 'Atiq', 'Developpement digital', 'ADMIN'),
+    ('louise.amaladasse@disney.com', 'Louise', 'Amaladasse', 'Developpement digital', 'USER'),
+    ('alba.avesada.calderon@disney.com', 'Alba', 'Avesada Calderon', 'Dining', 'USER'),
+    ('cyril.macaluso@disney.com', 'Cyril', 'Macaluso', 'Dining', 'USER'),
+    ('marie.dupont@disney.com', 'Marie', 'Dupont', 'Developpement digital', 'USER')
+    ON CONFLICT (email) DO NOTHING;
