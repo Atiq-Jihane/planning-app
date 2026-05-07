96ea75b1e2f87d4e2b3c6ffd68c29ab3506d12ec
ATIQ <jihane.atiq@disney.com>
2026-05-07 13:31:40 +0200

    fix data.sql init

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 MERGE INTO users (email, prenom, nom, equipe, role) KEY(email)
     VALUES
-    ('jihane.atiq@disney.com',           'Jihane', 'Atiq',             'Développement digital', 'ADMIN'),
-    ('louise.amaladasse@disney.com',     'Louise', 'Amaladasse',       'Développement digital', 'USER'),
+    ('jihane.atiq@disney.com',           'Jihane', 'Atiq',             'Developpement digital', 'ADMIN'),
+    ('louise.amaladasse@disney.com',     'Louise', 'Amaladasse',       'Developpement digital', 'USER'),
     ('alba.avesada.calderon@disney.com', 'Alba',   'Avesada Calderon', 'Dining',                'USER'),
     ('cyril.macaluso@disney.com',        'Cyril',  'Macaluso',         'Dining',                'USER'),
-    ('marie.dupont@disney.com',          'Marie',  'Dupont',           'Développement digital', 'USER');
+    ('marie.dupont@disney.com',          'Marie',  'Dupont',           'Developpement digital', 'USER');
